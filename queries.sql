06211ab0801153c158c5822e8786d1b636c85e57
Bess <c12641116@gmail.com>
2021-08-16 11:33:10 -0500

    add queries.sql of creating retirement_info

---
+++ queries.sql
@@ -0,0 +1,20 @@
+-- Retirement eligibility
+select first_name, last_name
+from employees
+where (birth_date between '1952-01-01' and '1955-12-31')
+and (hire_date between '1985-01-01' and '1988-12-31' );
+
+-- Number of employees retiring
+select count(first_name)
+from employees
+where (birth_date between '1952-01-01' and '1955-12-31')
+and (hire_date between '1985-01-01' and '1988-12-31' );
+
+-- Save as new table
+select first_name, last_name
+into retirement_info
+from employees
+where (birth_date between '1952-01-01' and '1955-12-31')
+and (hire_date between '1985-01-01' and '1988-12-31' );
+
+
